4e1181c76e9f5211ee6425333a5b6c93368769e0
KARAN KUMAR <kkvmalhi@gmail.com>
2023-05-30 22:53:58 +0500

    trello app(Mob V remains)

---
+++ js/script.js
@@ -0,0 +1,73 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let todoLane = document.getElementById("todo");
+let draggables = document.querySelectorAll(".task");
+let droppables = document.querySelectorAll(".dragDrop");
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let value = input.value;
+
+  if (!value) return;
+  let newTask = document.createElement("p");
+  newTask.classList.add("task");
+  newTask.setAttribute("draggable", "true");
+  newTask.innerText = value;
+
+  newTask.addEventListener("dragstart", () => {
+    newTask.classList.add("is-dragging");
+  });
+
+  newTask.addEventListener("dragend", () => {
+    newTask.classList.remove("is-dragging");
+  });
+
+  todoLane.appendChild(newTask);
+
+  input.value = "";
+});
+
+draggables.forEach((task) => {
+  task.addEventListener("dragstart", () => {
+    task.classList.add("is-dragging");
+  });
+  task.addEventListener("dragend", () => {
+    task.classList.remove("is-dragging");
+  });
+});
+
+droppables.forEach((zone) => {
+  zone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+
+    let bottomTask = insertAboveTask(zone, e.clientY);
+    let curTask = document.querySelector(".is-dragging");
+
+    if (!bottomTask) {
+      zone.appendChild(curTask);
+    } else {
+      zone.insertBefore(curTask, bottomTask);
+    }
+  });
+});
+
+let insertAboveTask = (zone, mouseY) => {
+  let els = zone.querySelectorAll(".task:not(.is-dragging)");
+
+  let closestTask = null;
+  let closestOffset = Number.NEGATIVE_INFINITY;
+
+  els.forEach((task) => {
+    let { top } = task.getBoundingClientRect();
+
+    let offset = mouseY - top;
+
+    if (offset < 0 && offset > closestOffset) {
+      closestOffset = offset;
+      closestTask = task;
+    }
+  });
+
+  return closestTask;
+};
